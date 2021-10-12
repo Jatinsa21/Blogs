a3a0a6f8ecefc8304d2562361022e0f8d92dc822
Jatin <jatin.s@tunicalabsmedia.com>
2021-10-12 12:35:25 +0530

    home 1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,8 @@ import { useHistory ,Link} from "react-router-dom";
 export const Home = () => {
     const [blog,setBlog] = useState([])
     const[all,setAll]= useState([blog])
+    const [searchInput, setSearchInput]=useState("")
+    
 
     function setFilterTech(){
         setAll(blog.filter(el =>el.Type == "Technology"))
@@ -12,9 +14,12 @@ export const Home = () => {
     function setFilterMarcket(){
         setAll(blog.filter(el =>el.Type == "Marketing"))
     }
-    function setFilterAll(){
+     function setFilterAll(){
         setAll(blog)
     }
+
+    const filterSearch = all.filter(el=>el.Heading?.toLowerCase().includes(searchInput?.toLowerCase()) )
+   
     useEffect(()=>{
         fetch(`${process.env.REACT_APP_HOST}/blogs`, {
         method: 'GET',
@@ -22,6 +27,7 @@ export const Home = () => {
       })
         .then(result => result.json())
         .then(result => {
+            console.log(result)
            setBlog(result)
            setAll(result)
         })
@@ -30,16 +36,17 @@ export const Home = () => {
         })
       
     },[])
+
     return (
-        <>{console.log(blog)}
-            {console.log(all)}
+        <>
+            
             <div className={classes.parent} >
                 <div className={classes.noch}>
                     <div className={classes.type}>
                         <div className={classes.button}>
                             <button onClick={setFilterAll} >All</button>
                         </div>
-                        <div className={classes.button}>
+                        <div className={classes.button1} >
                             <button onClick={setFilterTech}>Technology</button>
                         </div>
                         <div className={classes.button}>
@@ -48,64 +55,46 @@ export const Home = () => {
                     </div>
                     <div className={classes.logout}>
                         <Link className={classes.logoutBtn} to='/'>
-                            <img src="https://res.cloudinary.com/ditkixi88/image/upload/v1633936110/logout_f31dfc5571.png" />
+                            <img src="https://res.cloudinary.com/ditkixi88/image/upload/v1634018656/transparent_web_interface_icons_icon_logout_icon_5f8bbf9bc2f138_8715297616029940757985_18b1071046.png" />
                         </Link>
                     </div>
                 </div>
                 <div className={classes.search}>
-                    <input className={classes.input} placeholder="Search blog here" ></input>
+                    <input className={classes.input} placeholder="Search blog here" onChange={e=>setSearchInput(e.target.value)}  value={searchInput} ></input>
                 </div>
                 <div className={classes.data}>
                     <div className={classes.cards}>
-                        {all.map(ell => (
+                        {filterSearch.map((ell) => (
+                            
+                            ell ? (<>
                             <div className={classes.card}>
-                            <div className={classes.img}>
-                                {ell.BlogImg?<img src={ell.BlogImg.url}/>:null}
-                            </div>
-                            <div className={classes.detalis}>
-                              <div className={classes.head}>{ell.Heading}</div>
-                              <div className={classes.description}>{ell.Description}</div>
-                              <div className={classes.open}><button>Read full blog</button></div>
-                              <div className={classes.mid}>
-                                <div className={classes.author}>{ell.Author}</div>
-                                <div className={classes.types}>{ell.Type}</div>
-                              </div>
-                              <div className={classes.time}>{ell.readTime} <span>read</span></div>
-
-                            </div>
-                            </div>
-                         ))}
+                             <div className={classes.img}>
+                                 {ell.BlogImg?<img src={ell.BlogImg.url}/>:null}
+                             </div>
+                             <div className={classes.detalis}>
+                               <div className={classes.head}>{ell.Heading}</div>
+                               <div className={classes.description}><p>{ell.Description}</p></div>
+                               <div className={classes.bottom}>
+                               <div className={classes.open}><button>Read full blog</button></div>
+                               <div className={classes.mid}>
+                                 <div className={classes.author}>{ell.Author}</div>
+                                 <div className={classes.types}>{ell.Type}</div>
+                               </div>
+                               <div className={classes.time}>{ell.readTime} <span>read</span></div>
+                                 </div>
+                             </div>
+                             </div>
+                            </>):null
+ 
+                        )
+                        )}
                     </div>
 
                 </div>
+                {/* <div className={classes.end}>
+                    No more Results
+                </div> */}
             </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-               {/* {blog.filter(el =>el.Type == "Technology").map(ell => (
-                 <div className="card">
-                 <div className="content">
-                 <div className="img">
-                 {ell.BlogImg?<img src={`${process.env.REACT_APP_HOST}+${ell.BlogImg.url}`}/>:null}
-                 </div>
-                 <div className="details">
-                     <div className="name">{ell.Heading}</div>
-                     <div className="type">{ell.Type}</div>
-                 </div>
-                 </div>
-                 </div>   
-                 ))} */}
-
 
     </>
     )
