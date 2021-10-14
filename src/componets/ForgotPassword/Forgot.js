25d796419199ad3f7056a63042beaadd40a5de2d
Jatin <jatin.s@tunicalabsmedia.com>
2021-10-14 12:24:34 +0530

    .

--- src/componets/ForgotPassword/Forgot.js
+++ src/componets/ForgotPassword/Forgot.js
@@ -32,6 +32,7 @@ export default function Forgot() {
          <div className={classes.form}>
              <img  src={"https://res.cloudinary.com/ditkixi88/image/upload/v1633754999/icons8_new_view_80_d0ff370d68.png"}/>
              <div  className={classes.internal}>
+                 
              <p className={classes.head}>Reset Password</p>
              <div className={classes.left}>
                 <p className={classes.mesg}> Enter your email to reset your password.</p>
